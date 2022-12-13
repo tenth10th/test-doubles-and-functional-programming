52f707c7277deb1f5350573ee067396a69fab313
David Sturgis <david-sturgis@pluralsight.com>
2022-12-13 00:52:57 +0000

    Push some fixes to our initial state (whoops!)

--- writing_better_tests.py
+++ writing_better_tests.py
@@ -8,13 +8,16 @@ DEFAULT_STANDOFF = 5
 def handle_rate_limit(api_response: Dict) -> bool:
     """
     If our API response was rate limited, wait for the specified standoff time
+    (which is specified as a string, representing seconds to wait, if present)
 
     Return True if a rate limit occurred, otherwise False
     """
     seconds_str = api_response.get('standoff_time', '')
+    seconds_int = None
 
     try:
-        seconds_int = int(seconds_str)
+        if seconds_str:
+            seconds_int = int(seconds_str)
     except (ValueError):
         print(f"Invalid standoff_time? Defaulting to {DEFAULT_STANDOFF} seconds")
         seconds_int = DEFAULT_STANDOFF
